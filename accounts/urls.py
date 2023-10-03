e0505f88e99ed47c8889a2746d10ca550a8b290c
zablon <zablonoigo51@gmail.com>
2023-10-03 13:58:22 +0300

    accounts url namespace

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import (sign_in, sign_out, sign_up)
+app_name='accounts'
 urlpatterns=[
     path('login/', sign_in, name='login'),
     path('logout/', sign_out, name='logout'),
--- myshop_project/urls.py
+++ myshop_project/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('cart/',include('cart.urls', namespace='cart')),
     path('orders/', include('orders.urls', namespace='orders')),
+    path('accounts/', include('accounts.urls', namespace='accounts')),
     path('', include('shop.urls', namespace='shop')),
 ]
 
