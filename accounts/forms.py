0a391aa0965841d0de5e7a73d308787a80d3a030
zablon <zablonoigo51@gmail.com>
2023-10-05 08:31:36 +0300

    styling login and sign up page

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,10 +5,12 @@ from django.contrib.auth.forms import UserCreationForm
 class LoginForm(forms.Form):
     username=forms.CharField(max_length=65,widget=forms.TextInput(attrs={
         'placeholder':'Enter Username',
+        'class':'w-full rounded-lg px-6 py-4 border border-solid border-gray-600'
     }) )
     password=forms.CharField(max_length=65, widget=forms.PasswordInput(
         attrs={
             'placeholder':'Enter Password',
+            'class':'w-full rounded-lg px-6 py-4 border border-solid border-gray-600'
         }
         
     ))
@@ -17,6 +19,23 @@ class UserRegistrationForm(UserCreationForm):
     class Meta:
         model=User
         fields=['username','email','password1','password2']
+    
+    username=forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder':'Enter username for your account',
+        'class':'w-full px-6 py-4  rounded-lg border border-solid border-gray-600'
+    }))
+    email=forms.EmailField(widget=forms.EmailInput(attrs={
+        'placeholder':'Enter your Email Address',
+        'class':'w-full px-6 py-4  rounded-lg border border-solid border-gray-600'
+    }))
+    password1=forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder':'Enter Password',
+        'class':'w-full px-6 py-4  rounded-lg border border-solid border-gray-600'
+    }))
+    password2=forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder':'Repeat Password',
+        'class':'w-full px-6 py-4  rounded-lg border border-solid border-gray-600'
+    }))
 
 
     def clean_password(self):
