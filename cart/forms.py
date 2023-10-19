e8a337eea1b6f366289c1dffbc41f53abf6096ca
zablon <zablonoigo51@gmail.com>
2023-10-19 19:46:56 +0300

    changing max with on details page

--- cart/forms.py
+++ cart/forms.py
@@ -3,6 +3,7 @@ from django import forms
 PRODUCT_QUANTITY_CHOICES=[(i,str(i)) for i in range(1,21)]
 
 class CartAddProductForm(forms.Form):
-    quantity=forms.TypedChoiceField(choices=PRODUCT_QUANTITY_CHOICES, coerce=int)
+    quantity=forms.TypedChoiceField(choices=PRODUCT_QUANTITY_CHOICES, coerce=int,
+                                    widget=forms.Select(attrs={'class': 'border rounded px-4 py-2'}))
     override=forms.BooleanField(required=False,initial=False, widget=forms.HiddenInput)
     
