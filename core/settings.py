885e7dcb62c50562a7507f64a69092a799463dfe
zablon <zablonoigo51@gmail.com>
2024-09-04 17:44:55 +0300

    media and static base directory

--- core/settings.py
+++ core/settings.py
@@ -130,9 +130,9 @@ STRIPE_WEBHOOK_SECRET = env.str("STRIPE_WEBHOOK_SECRET")
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT= BASE_DIR / "staticfiles"
-MEDIA_URL="media/"
-MEDIA_ROOT=BASE_DIR/"media"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_URL = "media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 EMAIL_HOST = env.str("EMAIL_HOST")
 EMAIL_PORT = env.int("PORT")
 EMAIL_USE_TLS = env.bool("TLS")
