02c9ec0ba1da75d00af46ded047682fdad0f7224
Zablon Oigo <zablonoigo51@gmail.com>
2024-09-16 14:19:05 +0000

    Added vpc

--- infra/main.tf
+++ infra/main.tf
@@ -3,3 +3,8 @@ provider "aws" {
     access_key = "${var.access_key}"
     secret_key = "${var.secret_key}"
 }
+resource "aws_vpc" "production-vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+}
