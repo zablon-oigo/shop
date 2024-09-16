9f917bea3c232720120fc38461c1ce77637ab671
Zablon Oigo <zablonoigo51@gmail.com>
2024-09-16 14:21:04 +0000

    Added private subnet

--- infra/main.tf
+++ infra/main.tf
@@ -18,3 +18,13 @@ resource "aws_subnet" "public-subnet-2" {
   vpc_id            = aws_vpc.production-vpc.id
   availability_zone = var.availability_zones[1]
 }
+resource "aws_subnet" "private-subnet-1" {
+  cidr_block        = var.private_subnet_1_cidr
+  vpc_id            = aws_vpc.production-vpc.id
+  availability_zone = var.availability_zones[0]
+}
+resource "aws_subnet" "private-subnet-2" {
+  cidr_block        = var.private_subnet_2_cidr
+  vpc_id            = aws_vpc.production-vpc.id
+  availability_zone = var.availability_zones[1]
+}
