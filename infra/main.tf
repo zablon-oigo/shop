d9cf2c9db95d79df05459007ee221b8a4cabf2d4
Zablon Oigo <zablonoigo51@gmail.com>
2024-09-17 14:07:49 +0000

    Associate route table with subnet

--- infra/main.tf
+++ infra/main.tf
@@ -34,3 +34,7 @@ resource "aws_route_table" "public-route-table" {
 resource "aws_route_table" "private-route-table" {
   vpc_id = aws_vpc.production-vpc.id
 }
+resource "aws_route_table_association" "public-route-1-association" {
+  route_table_id = aws_route_table.public-route-table.id
+  subnet_id      = aws_subnet.public-subnet-1.id
+}
