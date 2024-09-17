2a6125a68d84414eb845f70d5f6ed3f0d27cdf67
Zablon Oigo <zablonoigo51@gmail.com>
2024-09-17 15:25:12 +0000

    Added private route table

--- infra/main.tf
+++ infra/main.tf
@@ -42,3 +42,7 @@ resource "aws_route_table_association" "public-route-2-association" {
   route_table_id = aws_route_table.public-route-table.id
   subnet_id      = aws_subnet.public-subnet-2.id
 }
+resource "aws_route_table_association" "private-route-1-association" {
+  route_table_id = aws_route_table.private-route-table.id
+  subnet_id      = aws_subnet.private-subnet-1.id
+}
