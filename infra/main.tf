8befb2061b7c713c6fd6a2eb656c88479673a376
Zablon Oigo <zablonoigo51@gmail.com>
2024-09-17 15:24:04 +0000

    Route table association

--- infra/main.tf
+++ infra/main.tf
@@ -38,3 +38,7 @@ resource "aws_route_table_association" "public-route-1-association" {
   route_table_id = aws_route_table.public-route-table.id
   subnet_id      = aws_subnet.public-subnet-1.id
 }
+resource "aws_route_table_association" "public-route-2-association" {
+  route_table_id = aws_route_table.public-route-table.id
+  subnet_id      = aws_subnet.public-subnet-2.id
+}
