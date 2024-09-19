e6a48716d15f8eb852dffe1bec58862634ff08ff
zablon <zablonoigo51@gmail.com>
2024-09-19 06:54:47 +0300

    Added health check

--- infra/main.tf
+++ infra/main.tf
@@ -76,5 +76,14 @@ resource "aws_alb_target_group" "default-target-group"{
     name     = "${var.ecs_cluster_name}-tg"
     port     = 80
     protocol = "HTTP"
+    health_check {
+    path                = var.health_check_path
+    port                = "traffic-port"
+    healthy_threshold   = 5
+    unhealthy_threshold = 2
+    timeout             = 2
+    interval            = 5
+    matcher             = "200"
+  }
 
 }
