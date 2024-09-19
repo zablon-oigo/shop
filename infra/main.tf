4703e8db56d6af9a1894f28bc0aa84954fbf8226
zablon <zablonoigo51@gmail.com>
2024-09-19 06:56:42 +0300

    Added default action listener

--- infra/main.tf
+++ infra/main.tf
@@ -92,4 +92,8 @@ resource "aws_alb_listener" "ecs-alb-http-listener" {
   port              = "80"
   protocol          = "HTTP"
   depends_on        = [aws_alb_target_group.default-target-group]
+    default_action {
+    type             = "forward"
+    target_group_arn = aws_alb_target_group.default-target-group.arn
+  }
 }
