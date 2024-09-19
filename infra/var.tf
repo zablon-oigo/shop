feed9c6bc1a07ae9c43cbbd71c15c19f76f57199
zablon <zablonoigo51@gmail.com>
2024-09-19 06:57:57 +0300

    added cluster name

--- infra/var.tf
+++ infra/var.tf
@@ -23,3 +23,7 @@ variable "health_check_path" {
   description = "Health check path for the default target group"
   default     = "/ping/"
 }
+variable "ecs_cluster_name" {
+  description = "Name of the ECS cluster"
+  default     = "production"
+}
