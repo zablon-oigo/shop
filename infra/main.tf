8fae197434d4714537a27964bd882aa6a060d2f4
zablon <zablonoigo51@gmail.com>
2024-09-20 14:38:24 +0300

    Added ecs task execution policy

--- infra/main.tf
+++ infra/main.tf
@@ -101,3 +101,8 @@ resource "aws_iam_role" "ecs-task-execution-role" {
   name               = "ecs_task_execution_role_prod"
   assume_role_policy = file("policies/ecs-role.json")
 }
+resource "aws_iam_role_policy" "ecs-task-execution-role-policy" {
+  name   = "ecs_task_execution_role_policy"
+  policy = file("policies/ecs-task-execution-policy.json")
+  role   = aws_iam_role.ecs-task-execution-role.id
+}
