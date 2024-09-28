701659e89f381c0094e4ff67cdf1f3fe213bc719
zablon <zablonoigo51@gmail.com>
2024-09-28 14:54:52 +0300

    Added recommender class

--- shop/recommender.py
+++ shop/recommender.py
@@ -1,1 +1,10 @@
 import redis
+from django.conf import settings
+from .models import Product
+
+
+r=redis.Redis(host=settings.REDIS_HOST, port=settings.REDIS_PORT,db=settings.REDIS_DB)
+
+class Recommender:
+    def get_product_key(self,id):
+        return f'product:{id}:purchased_with'
