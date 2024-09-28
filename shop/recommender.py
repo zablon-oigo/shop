cb63162e0fb35101137a1f3519f5427ed30b89d7
zablon <zablonoigo51@gmail.com>
2024-09-28 15:07:18 +0300

    getting list of product by their id

--- shop/recommender.py
+++ shop/recommender.py
@@ -8,3 +8,9 @@ r=redis.Redis(host=settings.REDIS_HOST, port=settings.REDIS_PORT,db=settings.RED
 class Recommender:
     def get_product_key(self,id):
         return f'product:{id}:purchased_with'
+    def products_bought(self, products):
+        product_ids=[p.id for p in products]
+        for product_id in product_ids:
+            for with_id in product_ids:
+                if product_id != with_id:
+                    r.zincrby(self.get_product_key(product_id), 1, with_id)
