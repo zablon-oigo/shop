08e04e93b1c3000e80701b9ad59aba59345890e7
zablon <zablonoigo51@gmail.com>
2024-09-29 12:54:38 +0300

    clear recommendations

--- shop/recommender.py
+++ shop/recommender.py
@@ -29,3 +29,6 @@ class Recommender:
         suggested_products=list(Product.objects.filter(id__in=suggested_products_ids))
         suggested_products.sort(key=lambda x: suggested_products_ids.index(x.id))
         return suggested_products
+    def clear_purchase(self):
+        for id in Product.objects.values_list('id',flat=True):
+            r.delete(self.get_product_key(id))
