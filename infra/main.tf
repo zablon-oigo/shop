418137473a98024c0b5752893246bec99e376d6a
zablon <zablonoigo51@gmail.com>
2024-10-03 17:56:06 +0300

    moved route to vpc module

--- infra/main.tf
+++ infra/main.tf
@@ -3,31 +3,8 @@ provider "aws" {
     access_key = "${var.access_key}"
     secret_key = "${var.secret_key}"
 }
-resource "aws_vpc" "production-vpc" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_support   = true
-  enable_dns_hostnames = true
-}
-resource "aws_subnet" "public-subnet-1" {
-  cidr_block        = var.public_subnet_1_cidr
-  vpc_id            = aws_vpc.production-vpc.id
-  availability_zone = var.availability_zones[0]
-}
-resource "aws_subnet" "public-subnet-2" {
-  cidr_block        = var.public_subnet_2_cidr
-  vpc_id            = aws_vpc.production-vpc.id
-  availability_zone = var.availability_zones[1]
-}
-resource "aws_subnet" "private-subnet-1" {
-  cidr_block        = var.private_subnet_1_cidr
-  vpc_id            = aws_vpc.production-vpc.id
-  availability_zone = var.availability_zones[0]
-}
-resource "aws_subnet" "private-subnet-2" {
-  cidr_block        = var.private_subnet_2_cidr
-  vpc_id            = aws_vpc.production-vpc.id
-  availability_zone = var.availability_zones[1]
-}
+
+
 resource "aws_route_table" "public-route-table" {
   vpc_id = aws_vpc.production-vpc.id
 }
