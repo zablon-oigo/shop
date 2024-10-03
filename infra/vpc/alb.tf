a7ab73d91b885286d96cd6013d2678f8f8a02504
zablon <zablonoigo51@gmail.com>
2024-10-03 20:20:56 +0300

    load balancer config

---
+++ infra/vpc/alb.tf
@@ -0,0 +1,7 @@
+resource "aws_lb" "production" {
+  name               = "${var.ecs_cluster_name}-alb"
+  load_balancer_type = "application"
+  internal           = false
+  security_groups    = [aws_security_group.load-balancer.id]
+  subnets            = [aws_subnet.public-subnet-1.id, aws_subnet.public-subnet-2.id]
+}
