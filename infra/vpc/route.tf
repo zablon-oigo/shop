8d52bb23ee8e9e436bb00b12926f36e3b11cc098
zablon <zablonoigo51@gmail.com>
2024-10-03 20:24:34 +0300

    associating public subnet with route table

--- infra/vpc/route.tf
+++ infra/vpc/route.tf
@@ -20,3 +20,8 @@ resource "aws_route_table_association" "private-route-2-association" {
   route_table_id = aws_route_table.private-route-table.id
   subnet_id      = aws_subnet.private-subnet-2.id
 }
+resource "aws_route" "public-internet-igw-route" {
+  route_table_id         = aws_route_table.public-route-table.id
+  gateway_id             = aws_internet_gateway.production-igw.id
+  destination_cidr_block = "0.0.0.0/0"
+}
