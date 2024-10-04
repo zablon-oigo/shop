d44a8f3aff48411f23e4c20b2680f322b5e0483b
zablon <zablonoigo51@gmail.com>
2024-10-04 17:48:38 +0300

    removed vpc variable

--- infra/var.tf
+++ infra/var.tf
@@ -1,29 +1,9 @@
-variable "public_subnet_1_cidr" {
-  description = "CIDR Block for Public Subnet 1"
-  default     = "10.0.1.0/24"
+variable "region" {
+    description = "default region"
 }
-variable "public_subnet_2_cidr" {
-  description = "CIDR Block for Public Subnet 2"
-  default     = "10.0.2.0/24"
+variable "access_key" {
+    description = "access key"
 }
-variable "private_subnet_1_cidr" {
-  description = "CIDR Block for Private Subnet 1"
-  default     = "10.0.3.0/24"
-}
-variable "private_subnet_2_cidr" {
-  description = "CIDR Block for Private Subnet 2"
-  default     = "10.0.4.0/24"
-}
-variable "availability_zones" {
-  description = "Availability zones"
-  type        = list(string)
-  default     = ["us-west-1b", "us-west-1c"]
-}
-variable "health_check_path" {
-  description = "Health check path for the default target group"
-  default     = "/ping/"
-}
-variable "ecs_cluster_name" {
-  description = "Name of the ECS cluster"
-  default     = "production"
+variable "secret_key" {
+    description = "secret key"
 }
