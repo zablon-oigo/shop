b9dff5de3b0aed23ab08754e43da5c9ad7118bf4
zablon <zablonoigo51@gmail.com>
2024-10-05 18:31:19 +0300

    Added vpc module and ecs module

--- infra/main.tf
+++ infra/main.tf
@@ -2,4 +2,11 @@ provider "aws" {
     region = "${var.region}"
     access_key = "${var.access_key}"
     secret_key = "${var.secret_key}"
+    token = "${var.token}"
+}
+module "vpc" {
+    source = "./vpc"
+}
+module "ecs" {
+    source = "./ecs"
 }
